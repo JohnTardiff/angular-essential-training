35ccab72c7b4ac71abf09d63cd5554ccb855e090
John Landon Tardiff <46567849+JohnTardiff@users.noreply.github.com>
2023-02-09 09:08:23 -0600

    completed 2.10

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { MediaItemComponent } from './media-item.component';
 
 @NgModule({
+<<<<<<< Updated upstream
     imports: [
         BrowserModule
     ],
@@ -12,5 +14,17 @@ import { AppComponent } from './app.component';
     bootstrap: [
         AppComponent
     ]
+=======
+  imports: [
+    BrowserModule
+  ],
+  declarations: [
+    AppComponent,
+    MediaItemComponent
+  ],
+  bootstrap: [
+    AppComponent
+  ]
+>>>>>>> Stashed changes
 })
 export class AppModule {}
